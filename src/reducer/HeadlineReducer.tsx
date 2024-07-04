bffdd79a622b5a6a3d354fa969df398e20665ef3
Luca Seara <caularase@gmail.com>
2024-07-04 18:03:43 -0300

    Add new features in the reducer, and error handlers

--- src/reducer/HeadlineReducer.tsx
+++ src/reducer/HeadlineReducer.tsx
@@ -15,6 +15,14 @@ export type initialStateType = {
     const verifySchema = v.safeParse(verifiedHeadlinesSchema, filteredArray) 
     return verifySchema
 }*/
+const filterValidHeadlines = (array: Headlines) => {
+    const filteredArray = array.filter(object => {
+        const allValuesValid = Object.values(object).every(value => value !== null && value !== undefined)
+        return allValuesValid
+    })
+    const verifySchema = v.safeParse(verifiedHeadlinesSchema, filteredArray)
+    return verifySchema
+}
 export const initialState: initialStateType = {
     headlines:[],
     error: false
@@ -22,15 +30,10 @@ export const initialState: initialStateType = {
 
 export const HeadlineReducer = (state: initialStateType = initialState, actions: Actions)=> {
     if(actions.type === 'get-top-headlines') { // Este action setea el state de topHeadlines desde una peticion fetch
-        //const newHeadlines = filterValidHeadlines(actions.payload.result)
-        const filteredArray = actions.payload.result.filter(object => {
-            const allValuesValid = Object.values(object).every(value => value !== null && value !== undefined);
-            return allValuesValid;
-        });
-        const verifySchema = v.safeParse(verifiedHeadlinesSchema, filteredArray)
+        const validHeadlines = filterValidHeadlines(actions.payload.result)
         return {
             ...state,
-            headlines: verifySchema.output
+            headlines: validHeadlines.output
         }
     }
     if(actions.type === 'set-error-in-top-headlines') { // Si falla el fetch mostramos un error en App.tsx
